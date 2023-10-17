8fd8a059dee282b6602e50addf1f9d4aa787406b
geisa <geisanebiu1@gmail>
2023-10-17 14:35:41 +0200

    added the functions of the input and button

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+    <form className="input" onSubmit={(e) => handleAdd(e)}>
+      <input
+        type="input"
+        value={todo}
+        onChange={(e: any) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input__box"
+      />
       <button className="input_submit" type="submit">
         Go
       </button>
